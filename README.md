4ff09544988f257e0c4247c7a7685f198e6ac268
Cameron <cameron@form.io>
2021-03-17 19:30:50 -0500

    Code

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# formio-stage-shaker
+# Description
+
+This script will purge any stages that are unconnected to projects, plus all related entries (to both the nonexistant project, and the stage being removed):
+
+- forms
+- roles
+- submissions
+
+If you wish to preserve any of these, you can comment out the relevant lines in `src/app.service.ts`. **Note:** this script only purges local mongo data. Please save the output of this script until you are sure the dust has settled.
+
+## Recommendations
+
+### Back up your database first
+
+This script doesn't delete data, but it's a whole lot easier to restore from a backup than send emergency emails to support for assistance.
+
+## How to use
+
+Go into `src/app.service.ts`, and fill in the connection options as indicated by comments
+
+```bash
+npm install
+npm start
+```
+
+If all goes correctly, it will list off all currently deleted projects. It will then flag all the attached stages as deleted also. Stages should immediately disappear from Portal UI
---
+++ src/app.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { MongoClient } from 'mongodb';
+
+@Injectable()
+export class AppService {
+  // This will be the same database name your portal uses
+  public static readonly DB_NAME = '';
+  public static readonly DB_CONNECT = 'mongodb://localhost:27017';
+
+  private async deleteMany(db, collection, args) {
+    console.log(`Delete ${collection} ${JSON.stringify(args)}`);
+    const results = await db.collection(collection).deleteMany(args);
+    console.log(`Results: ${results}`);
+  }
+
+  public async onModuleInit() {
+    const client = new MongoClient(AppService.DB_CONNECT, {
+      // Add any needed connection options here
+    });
+    client.connect(async (err) => {
+      if (err) {
+        console.error(err);
+        process.exit();
+      }
+      console.log('Connected successfully to server');
+
+      const db = client.db(AppService.DB_NAME);
+
+      const items = await db
+        .collection('projects')
+        .find({ type: 'project', deleted: { $ne: null } })
+        .toArray();
+
+      await Promise.all(
+        items.map(async (project) => {
+          console.log(`Cleanup: ${project._id} / ${project.title}`);
+          return db
+            .collection('projects')
+            .updateMany(
+              { project: project._id },
+              { $set: { deleted: project.deleted } },
+            );
+        }),
+      );
+
+      await client.close();
+    });
+  }
+}
